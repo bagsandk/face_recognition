c1a34ae0894f0d09d2e1bfb8741bea1537db5065
bagsandk_smp <b.sandoko@sinergimp.co.id>
2023-02-27 22:08:49 +0700

    crop

--- main_video.py
+++ main_video.py
@@ -44,10 +44,15 @@ def gen_frames():  # frameler şeklinde görüntüleri topluyoruz
             y1, x2, y2, x1 = face_loc[0], face_loc[1], face_loc[2], face_loc[3]
 
             # cv2.putText(frame, name,(x1, y1 - 10), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 200), 2)
-            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 200), 4)
-            ret, buffer = cv2.imencode('.jpg', frame)
+            width, height = 200, 200
+            x, y = x1, y1
+
+            crop_img = frame[y:y+height, x:x+width]
+            ret, buffer = cv2.imencode('.jpg', crop_img)
             face_base64 = base64.b64encode(buffer).decode()
             socketio.emit('updateSensorData', {'object':name, 'img':face_base64, 'date':get_current_datetime()})
+            
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 200), 4)
             
         socketio.sleep(1)
         
